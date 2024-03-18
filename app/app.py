ed9f1c41d0ffa79faf25b9639d9fa2a3bf23cb1e
danangyudhatama <danang.yudhatama@gmail.com>
2024-03-18 11:13:45 +0700

    edit app.py

--- app/app.py
+++ app/app.py
@@ -1,5 +1,31 @@
 # ----------------- Write your code below this line. -------------------- #
+import os
+from io import BytesIO
 
+from flask import Flask, render_template, request
+
+from config import config
+from hotdogclassifier import HotDogClassifier
+
+app = Flask(__name__)
+
+model = HotDogClassifier()
+model.load_model(config['model_weight'])
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template("index.html", flag=False, project_description=config["project_description"], project_name=config["project_name"])
+
+@app.route('/', methods=['POST'])
+def classify():
+    uploaded_file = request.files['files']
+    data = BytesIO(uploaded_file.read())
+
+    if uploaded_file.filename != "":
+        img, predicted = model.predict(data)
+    else:
+        img, predicted = '', ''
+    return render_template("index.html", predicted=predicted, img=img, flag=True, project_description=config["project_description"], project_name=config["project_name"])
 # ----------------- You do NOT need to understand what the code below does. -------------------- #
 
 if __name__ == '__main__':
